72c6e5e0e39a332a532c6a87df2b19443f554961
Enzo <chevenzo@hotmail.com>
2016-05-12 16:55:33 +0200

    Démarrage de l'algo FOIL

--- src/main.java
+++ src/main.java
@@ -7,4 +7,12 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
+	
+	//TODO : Créer un objet pour les enregistrements
+	
+	// En développement
+//	public void algoFOIL(List<Enregistrement> Pos, List<Enregistrement> Neg) {
+//		Regle mesRegles = new Regle();
+//		while (Pos.)
+//	}
 }
